9eda2077d070f6b2a03eec05ee25c4a347f0a453
omormehn <nathanuseni@gmail.com>
2026-02-05 03:54:41 +0100

    recompiled smart contract

--- blockchain.js
+++ blockchain.js
@@ -1,150 +1,157 @@
-const { connectDB } = require('./db');
-const sha256 = require('sha256');
-const crypto = require('crypto');
+const { connectDB } = require("./db");
+const sha256 = require("sha256");
+const crypto = require("crypto");
 
 class Blockchain {
-    constructor() {
-        this.chain = [];
-        this.pendingTransactions = [];
+  constructor() {
+    this.chain = [];
+    this.pendingTransactions = [];
 
-        // Genesis block
-        this.createNewBlock('0');
-    }
+    // Genesis block
+    this.createNewBlock("0");
+  }
 
-    // =========================
-    // BLOCKS (BLOCK LINKING)
-    // =========================
+  // =========================
+  // BLOCKS (BLOCK LINKING)
+  // =========================
 
-async createNewBlock(previousBlockHash) {
+  async createNewBlock(previousBlockHash) {
     const block = {
-        index: this.chain.length + 1,
-        timestamp: Date.now(),
-        transactions: this.pendingTransactions,
-        previousBlockHash,
-        hash: this.hashBlock(previousBlockHash, this.pendingTransactions)
-        };
+      index: this.chain.length + 1,
+      timestamp: Date.now(),
+      transactions: this.pendingTransactions,
+      previousBlockHash,
+      hash: this.hashBlock(previousBlockHash, this.pendingTransactions),
+    };
 
-        this.pendingTransactions = [];
-        this.chain.push(block);
+    this.pendingTransactions = [];
+    this.chain.push(block);
 
-        //  SAVE TO MONGODB
+    //  SAVE TO MONGODB
     const db = await connectDB();
-    await db.collection('blocks').insertOne(block);
+    await db.collection("blocks").insertOne(block);
 
     console.log(`Block ${block.index} saved to MongoDB`);
 
     return block;
+  }
+
+  //link the next block correctly
+  getLastBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  //block hash depends on previous block and all transactions
+  hashBlock(previousBlockHash, transactions) {
+    return sha256(
+      previousBlockHash + JSON.stringify(transactions), //we stringify it because hash functions only work on strings
+    );
+  }
+
+  // =========================
+  //  KEYS & SIGNATURES
+  // =========================
+
+  //Each participant in the supply chain has a cryptographic identity created using elliptic curve cryptography. They sign every scan with their private key, and anyone can verify the authenticity using the public key.
+  createKeyPair() {
+    return crypto.generateKeyPairSync("ec", {
+      namedCurve: "secp256k1",
+      publicKeyEncoding: { type: "spki", format: "pem" },
+      privateKeyEncoding: { type: "pkcs8", format: "pem" },
+    });
+  }
+
+  //announcing blame detection
+
+  signTransaction(hash, privateKey) {
+    const sign = crypto.createSign("SHA256");
+    sign.update(hash);
+    sign.end();
+    return sign.sign(privateKey, "hex");
+  }
+
+  verifyTransaction(transaction) {
+    const verify = crypto.createVerify("SHA256");
+    verify.update(transaction.hash);
+    verify.end();
+    return verify.verify(transaction.publicKey, transaction.signature, "hex");
+  }
+
+  //  TRANSACTIONS
+
+  createScanTransaction(
+    tvId,
+    location,
+    handler,
+    condition,
+    privateKey,
+    publicKey,
+  ) {
+    //  SMART CONTRACT CHECKS
+    if (!this.isValidNextScan(tvId, location, handler)) {
+      throw new Error(`Smart Contract Violation for ${tvId}`);
     }
 
-    //link the next block correctly
-    getLastBlock() {
-        return this.chain[this.chain.length - 1];
+    const transaction = {
+      tvId,
+      location,
+      handler,
+      condition,
+      timestamp: Date.now(),
+    };
+
+    transaction.hash = this.hashTransaction(transaction);
+
+
+    transaction.signature = this.signTransaction(transaction.hash, privateKey);
+    transaction.publicKey = publicKey;
+
+    this.pendingTransactions.push(transaction);
+    return transaction;
+  }
+
+  hashTransaction(transaction) {
+    return sha256(
+      transaction.tvId +
+        transaction.location +
+        transaction.handler +
+        transaction.condition +
+        transaction.timestamp,
+    );
+  }
+
+  //  SMART CONTRACT RULES
+
+  isValidNextScan(tvId, newLocation, newHandler) {
+    const locationOrder = ["Factory", "Truck", "Warehouse"];
+
+    //collects all transactions from all blocks and flattens them into a single list so smart contracts can check the full history then filters all transactions belonging to one tv only
+    const history = this.chain
+      .flatMap((block) => block.transactions)
+      .filter((tx) => tx.tvId === tvId);
+
+    // Rule 1: Must start at Factory
+    if (history.length === 0) {
+      return newLocation === "Factory";
     }
+    //The line below says give me the most recent transaction for this TV
+    const lastTx = history[history.length - 1];
 
-    //block hash depends on previous block and all transactions
-    hashBlock(previousBlockHash, transactions) {
-        return sha256(
-            previousBlockHash +
-            JSON.stringify(transactions) //we stringify it because hash functions only work on strings
-        );
-    }
+    // Rule 2: No scans after damage
+    if (lastTx.condition === "DAMAGED") return false;
 
-    // =========================
-    //  KEYS & SIGNATURES
-    // =========================
+    // Rule 3: Cannot scan same location twice
+    if (lastTx.location === newLocation) return false;
 
-    //Each participant in the supply chain has a cryptographic identity created using elliptic curve cryptography. They sign every scan with their private key, and anyone can verify the authenticity using the public key.
-    createKeyPair() {
-        return crypto.generateKeyPairSync('ec', {
-            namedCurve: 'secp256k1',
-            publicKeyEncoding: { type: 'spki', format: 'pem' },
-            privateKeyEncoding: { type: 'pkcs8', format: 'pem' }
-        });
-    }
+    // Rule 4: Same handler cannot scan twice
+    if (lastTx.handler === newHandler) return false;
 
-    //announcing blame detection
-
-    signTransaction(hash, privateKey) {
-        const sign = crypto.createSign('SHA256');
-        sign.update(hash);
-        sign.end();
-        return sign.sign(privateKey, 'hex');
-    }
-
-    verifyTransaction(transaction) {
-        const verify = crypto.createVerify('SHA256');
-        verify.update(transaction.hash);
-        verify.end();
-        return verify.verify(transaction.publicKey, transaction.signature, 'hex');
-    }
-
-    
-    //  TRANSACTIONS
-    
-
-    createScanTransaction(tvId, location, handler, condition, privateKey, publicKey) {
-
-        //  SMART CONTRACT CHECKS
-        if (!this.isValidNextScan(tvId, location, handler)) {
-            throw new Error(`Smart Contract Violation for ${tvId}`);
-        }
-
-        const transaction = {
-            tvId,
-            location,
-            handler,
-            condition,
-            timestamp: Date.now()
-        };
-
-        transaction.hash = this.hashTransaction(transaction);
-        transaction.signature = this.signTransaction(transaction.hash, privateKey);
-        transaction.publicKey = publicKey;
-
-        this.pendingTransactions.push(transaction);
-        return transaction;
-    }
-
-    hashTransaction(transaction) {
-        return sha256(
-            transaction.tvId +
-            transaction.location +
-            transaction.handler +
-            transaction.condition +
-            transaction.timestamp
-        );
-    }
-
-    //  SMART CONTRACT RULES
-
-    isValidNextScan(tvId, newLocation, newHandler) {
-        const locationOrder = ['Factory', 'Truck', 'Warehouse'];
-
-//collects all transactions from all blocks and flatens them into a single list so smart contracts can check the full history then filters all transactions belonging to one tv only
-        const history = this.chain
-            .flatMap(block => block.transactions)
-            .filter(tx => tx.tvId === tvId);
-
-        // Rule 1: Must start at Factory
-        if (history.length === 0) {
-            return newLocation === 'Factory';
-        }
-//The line below says give me the most recent transaction for this TV
-        const lastTx = history[history.length - 1];
-
-         // Rule 2: No scans after damage
-        if (lastTx.condition === 'DAMAGED') return false;
-
-        // Rule 3: Cannot scan same location twice
-        if (lastTx.location === newLocation) return false;
-
-        // Rule 4: Same handler cannot scan twice
-        if (lastTx.handler === newHandler) return false;
-
-        // Rule 1 continued: Correct order
-        return locationOrder.indexOf(newLocation) ===
-               locationOrder.indexOf(lastTx.location) + 1;
-    }
+    // Rule 1 continued: Correct order
+    return (
+      locationOrder.indexOf(newLocation) ===
+      locationOrder.indexOf(lastTx.location) + 1
+    );
+  }
 }
 
 module.exports = Blockchain;
